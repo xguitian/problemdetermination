d4790012e0a6be82e37a91947c915f350291b1ec
Kevin Grigorenko <kevin.grigorenko@us.ibm.com>
2014-10-15 17:43:48 -0700

    Support time zones in graphcsv.r

--- scripts/linux/linperf_vmstat.pl
+++ scripts/linux/linperf_vmstat.pl
@@ -1,16 +1,21 @@
 #!/usr/bin/env perl
 BEGIN {
   use Time::Piece;
-  print "Time,CPU,Runqueue,Blocked,MemoryFree,PageIns,ContextSwitches,Wait,Steal\n";
 }
 $line = $_;
 chomp($line);
 if ($line =~ /VMSTAT_INTERVAL = (\d+)$/) {
   $interval=$1;
 } elsif ($line =~ /^\w+ \w+ \d+ \d+:\d+:\d+ \w+ \d+$/) {
-  $line =~ s/^(.*) \S+( \d+)$/\1\2/;
+  $line =~ s/^(.*) (\S+)( \d+)$/\1\3/;
+  $tz = $2;
   $time = Time::Piece->strptime($line, "%a %b %e %H:%M:%S %Y");
-} elsif ($line =~ /^\s*(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)/) {
+  if (!$hasTime) {
+    $hasTime = 1;
+    print "Time ($tz),CPU,Runqueue,Blocked,MemoryFree,PageIns,ContextSwitches,Wait,Steal\n";
+  }
+} elsif ($hasTime && $line =~ /^\s*(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)/) {
+  # Ignore first one because `man vmstat`: "The first report produced gives averages since the last reboot."
   if ($first) {
     $time = $time + $interval;
     print $time->strftime("%Y-%m-%d %H:%M:%S") . "," . (100 - $15) . ",$1,$2," . ($4 + $5 + $6) . ",$7,$12,$16,$17\n";
