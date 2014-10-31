458380f075c0d97bfc7d49cfddc573ba39fe1627
Kevin Grigorenko <kevin.grigorenko@us.ibm.com>
2014-10-30 17:36:38 -0700

    Fix import error

--- scripts/java/jeesample/src/main/java/com/ibm/sample/Servlet1.java
+++ scripts/java/jeesample/src/main/java/com/ibm/sample/Servlet1.java
@@ -4,6 +4,7 @@ import java.io.*;
 import java.util.*;
 import javax.servlet.*;
 import javax.servlet.annotation.*;
+import javax.servlet.http.*;
 
 @WebServlet(name = "Servlet1", urlPatterns = "/servlet1")
 public class Servlet1 extends HttpServlet {
