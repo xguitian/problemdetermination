c43dd43529dc3f235aef23829cbdd6743b249a22
Kevin Grigorenko <kevin.grigorenko@us.ibm.com>
2014-10-30 18:17:40 -0700

    Fix WAS access log usage message

--- scripts/was/httpchannel.sh
+++ scripts/was/httpchannel.sh
@@ -2,13 +2,13 @@
 command -v perl >/dev/null 2>&1 || { echo >&2 "perl required"; exit 1; }
 command -v R >/dev/null 2>&1 || { echo >&2 "R required"; exit 1; }
 if [ $# -ne 2 ] || [ ! -f "$2" ]; then
-  echo "usage: accesslog.sh NCSALogFormat access.log. Example NCSALogFormat: \"%h %i %u %t \\\"%r\\\" %s %b %D\"";
+  echo "usage: accesslog.sh NCSALogFormat access.log. Example NCSALogFormat: \"%h %i %u %t \\\\\\\"%r\\\\\\\" %s %b %D\"";
   if [ ! -f "$2" ]; then echo "$2 not found"; fi
   exit 1;
 fi
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export LOGFORMAT="$1"
-if [ -z "$LOGFORMAT" ]; then echo "usage: accesslog.sh NCSALogFormat access.log. Example NCSALogFormat: \"%h %i %u %t \\\"%r\\\" %s %b %D\""; exit 1; fi
+if [ -z "$LOGFORMAT" ]; then echo "usage: accesslog.sh NCSALogFormat access.log. Example NCSALogFormat: \"%h %i %u %t \\\\\\\"%r\\\\\\\" %s %b %D\""; exit 1; fi
 echo "LogFormat: ${LOGFORMAT}"
 export INPUT_TITLE="access log ${2}"
 export INPUT_PNGFILE="${INPUT_TITLE//[^a-zA-Z0-9_]/}.png"
