bcbe7daf653e7d768d04c98e2a436609b5ef9a4c
Kevin Grigorenko <kevin.grigorenko@us.ibm.com>
2015-01-10 22:22:36 -0800

    plot.xts encountered bug in certain data sets

--- scripts/r/graphcsv.r
+++ scripts/r/graphcsv.r
@@ -44,6 +44,7 @@ if(usexts) {
     data,
     main=paste(title, " (Timezone ", timezone, ")", sep=""),
     minor.ticks=FALSE,
+    major.ticks="seconds",
     yax.loc="left",
     auto.grid=TRUE,
     nc=cols,
